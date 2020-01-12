1eb0a46eb0fed2a18307ee05895d58b0eef84a96
vantri0964 <vantri27051998@gmail.com>
2020-01-12 23:21:21 +0700

    init project

---
+++ src/main/java/levantri/dev/reportend/ReportendApplication.java
@@ -0,0 +1,13 @@
+package levantri.dev.reportend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/levantri/dev/reportend/ReportendApplicationTests.java
@@ -0,0 +1,13 @@
+package levantri.dev.reportend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
